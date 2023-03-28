acf400d8352414a51b71c1fb7cfff18d6ad7ba6b
realjf <real_jf@hotmail.com>
2023-03-28 11:24:30 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,51 @@
-#include <SDL.h>
-#include <stdio.h>
+#include <stdio.h> /* printf and fprintf */
+
+#ifdef _WIN32
+#include <SDL.h> /* Windows-specific SDL2 library */
+#else
+#include <SDL.h> /* macOS- and GNU/Linux-specific */
+#endif
+
+/* Sets constants */
+#define WIDTH 800
+#define HEIGHT 600
+#define DELAY 3000
 
 int main(int argc, char **argv) {
+    /* Initialises data */
+    SDL_Window *window = NULL;
 
-    SDL_Window* window;
-
-    SDL_Init(SDL_INIT_VIDEO);
-
-    window = SDL_CreateWindow(
-        "An SDL2 window",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        SDL_WINDOW_OPENGL
-    );
-
-    if (window == NULL) {
-        printf("Could not create window: %d\n", SDL_GetError());
+    /*
+     * Initialises the SDL video subsystem (as well as the events subsystem).
+     * Returns 0 on success or a negative error code on failure using SDL_GetError().
+     */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
         return 1;
     }
 
+    /* Creates a SDL window */
+    window = SDL_CreateWindow("SDL Example",           /* Title of the SDL window */
+                              SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+                              SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                              WIDTH,                   /* Width of the window in pixels */
+                              HEIGHT,                  /* Height of the window in pixels */
+                              0);                      /* Additional flag(s) */
 
+    /* Checks if window has been created; if not, exits program */
+    if (window == NULL) {
+        fprintf(stderr, "SDL window failed to initialise: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    /* Pauses all SDL subsystems for a variable amount of milliseconds */
+    SDL_Delay(DELAY);
+
+    /* Frees memory */
+    SDL_DestroyWindow(window);
+
+    /* Shuts down all SDL subsystems */
+    SDL_Quit();
+
+    return 0;
 }
